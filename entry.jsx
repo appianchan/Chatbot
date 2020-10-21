da64aaf10a79a88d5ba5cf0535e43d3a60001e77
Anthony Chan <appianchan@gmail.com>
2020-10-20 17:36:37 -0700

    used yo botkit to generate a bot

--- entry.jsx
+++ entry.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import botkit from 'botkit';
 
 
 let { Botkit } = require("botkit");
 
-const controller = new Botkit(MY_CONFIGURATION);
+// const controller = new Botkit(MY_CONFIGURATION);
 
-controller.hears("hello", "direct_message", function (bot, message) {
-  bot.reply(message, "Hello yourself!");
-});
+// controller.hears("hello", "direct_message", function (bot, message) {
+//   bot.reply(message, "Hello yourself!");
+// });
 
 function Root() {
   return (
     <div>
-      sup
+      buyzzz
     </div>
   );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<Root controller={controller}/>, document.getElementById("main"));
+  ReactDOM.render(<Root />, document.getElementById("main"));
 });
