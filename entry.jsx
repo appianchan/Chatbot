292033efd3749ee2489d987f58622043e8919bcc
Anthony Chan <appianchan@gmail.com>
2020-10-20 17:06:09 -0700

    pushing to github

--- entry.jsx
+++ entry.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+let { Botkit } = require("botkit");
+
+const controller = new Botkit(MY_CONFIGURATION);
+
+controller.hears("hello", "direct_message", function (bot, message) {
+  bot.reply(message, "Hello yourself!");
+});
+
 function Root() {
   return (
     <div>
-      hi
+      sup
     </div>
   );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<Root />, document.getElementById("main"));
+  ReactDOM.render(<Root controller={controller}/>, document.getElementById("main"));
 });
